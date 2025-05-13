b80f7c83e686dd6279faed8e89e368b9f62d75e7
VedroMan <123834638+VedroMan@users.noreply.github.com>
2025-05-13 23:25:04 +0300

    Добавил кнопки, подправил код

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { faDeleteLeft, faCancel } from "@fortawesome/free-solid-svg-icons";
+import { faDeleteLeft, faCancel, faComment, faCheck, faIcons, faMessage } from "@fortawesome/free-solid-svg-icons";
 
 const amount = ref("");
 
@@ -35,15 +35,12 @@ const formattedAmount = () => {
 
 <template>
   <div class="flex flex-col items-center justify-center px-4">
-    <!-- Заголовок -->
     <h1 class="text-xl font-bold">Введите сумму</h1>
 
-    <!-- Поле отображения суммы -->
     <div class="mt-15 text-3xl sm:text-4xl font-bold bg-white dark:bg-gray-800 dark:text-white text-gray-900 px-4 py-3 rounded-lg shadow w-full max-w-xs text-center">
       {{ formattedAmount() }}
     </div>
 
-    <!-- Цифровая клавиатура -->
     <div class="grid grid-cols-3 gap-3 mt-6 w-full max-w-xs">
       <button
         v-for="n in [1,2,3,4,5,6,7,8,9,0]"
@@ -55,13 +52,18 @@ const formattedAmount = () => {
       </button>
     </div>
 
-    <!-- Управляющие кнопки -->
     <div class="flex gap-4 mt-10 w-full max-w-xs justify-between">
-      <button @click="resetAmount" class="flex-1 bg-red-500 text-white py-3 rounded-lg text-xl hover:bg-red-600 transition">
-        <faicon :icon="faCancel" />
+      <button class="flex-1 bg-emerald-500 text-white py-3 rounded-lg text-xl hover:bg-emerald-600 transition">
+        <faicon :icon="faCheck" />
       </button>
       <button @click="deleteLast" class="flex-1 bg-gray-500 text-white py-3 rounded-lg text-xl hover:bg-gray-600 transition">
         <faicon :icon="faDeleteLeft" />
+      </button>
+      <button @click="resetAmount" class="flex-1 bg-red-500 text-white py-3 rounded-lg text-xl hover:bg-red-600 transition">
+        <faicon :icon="faCancel" />
+      </button>
+      <button class="flex-1 bg-blue-500 text-white py-3 rounded-lg text-xl hover:bg-blue-600 transition">
+        <faicon :icon="faMessage" />
       </button>
     </div>
   </div>
